45aaadb233261ec2aa47932f48f135799cd13958
seyedmahdihosseini06@gmail.com <seyedmahdihosseini06@gmail.com>
2025-12-25 13:08:26 +0330

    chore: format Solidity files with forge fmt

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -15,10 +15,7 @@ contract FundFundMe is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "FundMe",
-            block.chainid
-        );
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
         fundFundMe(mostRecentlyDeployed);
     }
 }
@@ -31,10 +28,7 @@ contract WithdrawFundMe is Script {
     }
 
     function run() external {
-        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
-            "FundMe",
-            block.chainid
-        );
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
         withdrawFundMe(mostRecentlyDeployed);
     }
 }
